e5b6b7976764574c7adf6c14e558c25599e4d913
Hamza Munaf <hamzamunaf@10-249-27-69.wireless.oregonstate.edu>
2019-12-10 21:05:34 -0800

    rendering pages

--- server.js
+++ server.js
@@ -10,4 +10,23 @@ app.engine('handlebars', exphbs({
   layoutsDir: path.join(__dirname, "views/layouts/")}));
 
   app.set('view engine', 'handlebars');
-  
+
+  app.get('/', function(req, res){
+    res.status(200).render('mainpage');
+  });
+
+  app.get('/photos', function(req, res){
+    res.status(200).render('photos');
+  });
+
+  app.get('/categories', function(req, res){
+    res.status(200).render('photos');
+  });
+
+
+app.use(express.static('public'));
+
+
+app.listen(port, function () {
+  console.log("== Server is listening on port", port);
+});
